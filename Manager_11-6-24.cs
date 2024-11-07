e6e82f7e4e10055815f50c46fa1c36fba3f44fd9
DorineAka <122583447+DorineAka@users.noreply.github.com>
2024-11-07 14:18:38 -0600

    Update Manager_11-6-24.cs

--- Manager_11-6-24.cs
+++ Manager_11-6-24.cs
@@ -71,5 +71,47 @@ namespace InvManager
             // given an ID, if any parameter is not null, overide that items field for corresponding parameter
         }
         
+        public static bool UpdateItemQuantity(this InventoryManager manager , string sku, int newQuantity)
+    {
+        //Method to update the quantity of an item by sku
+        InventoryItem item = manager.FindItemBySku(sku);
+
+        if(item != null)
+        {
+            item.Quantity = newQuantity;
+            return true;
+        }
+        Console.WriteLine("Item not found");
+        return false;
+    }
+    public static bool UpdateItemQuantity(this InventoryManager manager, string sku, int newPrice)
+    {
+        //Method to update the price of an item by sku
+        InventoryItem item = manager.FindItemBySku(sku);
+
+        if (item != null)
+        {
+            item.Price = newPrice;
+            return true;
+        }
+        Console.WriteLine("Item not found");
+        return false;
+
+    }
+    public static bool UpdateItemName(this InventoryManager manager, string sku, string newName)
+    {
+        //Method to update the name of an item by sku
+        InventoryItem item = manager.FindItemBySku(sku);
+
+        if (item != null)
+        {
+            item.Name = newName;
+            return true;
+        }
+        Console.WriteLine("Item not found");
+        return false;
+
+    }
+        
     }
 }
